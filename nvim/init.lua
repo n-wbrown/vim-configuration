f932d3a9de51f96a1649246eaf971a2510a77122
n-wbrown <n-wbrown@users.noreply.github.com>
2023-03-03 11:20:05 -0800

    bring back tree-sitter hl, update sitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -23,7 +23,7 @@ require'nvim-treesitter.configs'.setup {
     -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
     highlight = {
-        enable = false,
+        enable = true,
 
         -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
         -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
@@ -43,7 +43,7 @@ require'nvim-treesitter.configs'.setup {
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
         -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
+        additional_vim_regex_highlighting = true,
     },
 }
 
